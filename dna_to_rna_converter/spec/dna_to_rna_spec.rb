c35925acf9c14b8ab2b3cd482f4bb71c9ed1829d
Amber Johnson <amberjohnsonsmile@gmail.com>
2022-06-10 11:18:24 -0600

    Add solution and spec files

---
+++ dna_to_rna_converter/spec/dna_to_rna_spec.rb
@@ -0,0 +1,9 @@
+require "./lib/dna_to_rna"
+
+describe "Solution" do
+  it "Fixed tests" do
+    Test.assert_equals(dna_to_rna("TTTT"), "UUUU")
+    Test.assert_equals(dna_to_rna("GCAT"), "GCAU")
+    Test.assert_equals(dna_to_rna("GACCGCCGCC"), "GACCGCCGCC")
+  end
+end
